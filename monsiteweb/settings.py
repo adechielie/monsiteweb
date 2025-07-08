660b68967d1ea32cc102cd9e0ed071974793fa6b
adechielie <adechielie@gmail.com>
2025-07-08 20:27:20 +0200

    modification de allowed_host

--- monsiteweb/settings.py
+++ monsiteweb/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['todoba.net', 'www.todoba.net','127.0.0.1']
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='').split(',')
 
 
 # Application definition
