5bda61b80a6864fe25abe1499bc9355048aa6890
adechielie <adechielie@gmail.com>
2025-07-08 13:27:52 +0200

    test de déploiement automatique 8

--- monsiteweb/settings.py
+++ monsiteweb/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-jh2-8_0ag(*+@ezq!mf+devihru-cqz*jeivrjr24%l6nv0gz*'
+SECRET_KEY = config('DJANGO_SECRET_KEY')  
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
