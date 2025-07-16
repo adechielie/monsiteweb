725596730cf13b8290b9110245a0aee562643742
adechielie <adechielie@gmail.com>
2025-07-16 15:57:21 +0200

    rajout de l'email

--- pages/urls.py
+++ pages/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from .views import home, about
+from .views import test_email
 
 urlpatterns = [
     path('', home, name='home'),
     path('about/', about, name='about'),
+    path('test-email/', test_email, name='test_email'),
 ]
