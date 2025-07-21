9f4ea1758c3df75158b2f581a184645024c18e77
adechielie <adechielie@gmail.com>
2025-07-21 03:28:08 +0200

    modif de services et env

--- pages/urls.py
+++ pages/urls.py
@@ -1,9 +1,7 @@
 from django.urls import path
 from .views import home, about
-from .views import test_email
 
 urlpatterns = [
     path('', home, name='home'),
     path('about/', about, name='about'),
-    path('test-email/', test_email, name='test_email'),
 ]
--- pages/views.py
+++ pages/views.py
@@ -5,16 +5,3 @@ def home(request):
 
 def about(request):
     return render(request, 'about.html')
-
-from django.core.mail import send_mail
-from django.http import HttpResponse
-
-def test_email(request):
-    send_mail(
-        'Test de mail depuis Django',
-        'Si tu lis ceci, le mail est bien parti !',
-        config('EMAIL_USER'),  # <-- tu peux mettre config('EMAIL_USER') aussi
-        [config('EMAIL_USER')],
-        fail_silently=False,
-    )
-    return HttpResponse("E-mail envoyé !")
